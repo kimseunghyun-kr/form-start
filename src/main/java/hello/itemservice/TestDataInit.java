42e012b3c983640a4a95f5ffa84374c61e320a2d
kimseunghyun-kr <incongnito12@gmail.com>
2023-06-13 16:50:40 +0800

    init2

---
+++ src/main/java/hello/itemservice/TestDataInit.java
@@ -0,0 +1,24 @@
+package hello.itemservice;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final ItemRepository itemRepository;
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
